22e13106fd82d920a46dd3fae861681589be72b3
Tomass Yoga <tomass.petersons96@gmail.com>
2022-04-21 19:23:31 +0300

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function () {
+  let args = Array.from(arguments);
+  let array = args[0];
 
-};
-
+  for (let i = 1; i < args.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      if (array[j] === args[i]) {
+        array.splice(j, 1);
+      }
+    }
+  }
+  return array;
+}
 // Do not edit below this line
 module.exports = removeFromArray;
