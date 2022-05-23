d661d9d8bbccd8aaefc26acface2625bd57c6895
Tomass Yoga <tomass.petersons96@gmail.com>
2022-05-23 14:57:49 +0300

    Fix "calculator.js" Task

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,46 +1,42 @@
-const add = function(a, b) {
-	return a + b;
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function(a, b) {
-	return a - b;
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function(array) {
-	let sum = array.reduce(function (previousValue, currentValue) {
+const sum = function (array) {
+  let sum = array.reduce(function (previousValue, currentValue) {
     return previousValue + currentValue;
   }, 0);
   return sum;
 };
 
-const multiply = function(array) {
+const multiply = function (array) {
   let multiply = array.reduce(function (previousValue, currentValue) {
     return previousValue * currentValue;
   });
   return multiply;
 };
 
-const power = function(a, b) {
-  let power = a;
-  for (let i = 1; i < b; i++) {
-    power *= a;
-  };
-  return power;
+const power = function (a, b) {
+  // let power = a;
+  // for (let i = 1; i < b; i++) {
+  //   power *= a;
+  // };
+  // return power;
+
+  return Math.pow(a, b);
 };
 
-const factorial = function(a) {
-  let result = 0;
-  let number = a;
-  if (number === 0) {
-    result = 1;
-  } else {
-    number = 1;
-    for (let i = 1; i <= a; i++) {
-      number *= i;
-    }
-    result = number; 
+const factorial = function (a) {
+  if (a === 0) return 1;
+  number = 1;
+  for (let i = 1; i <= a; i++) {
+    number *= i;
   }
-  return result;
+  return number;
 };
 
 // Do not edit below this line
