b8f443881cbb18a6afb2fbd7b97b5e375f8c18ee
Tomass Yoga <tomass.petersons96@gmail.com>
2022-05-23 17:47:48 +0300

    Complete "fibonacci.spec.js" task

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+  let array = [1,1];
+  let number = 0;
+  index = parseInt(index);
+  if (index <= 0) return "OOPS";
+  for (i=1; i <= index; i++) {
+    number = array[i-1] + array[i];
+    array.push(number);
+  }
+  return array[index - 1];
 };
 
 // Do not edit below this line
