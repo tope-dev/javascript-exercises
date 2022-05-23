c1c3e58ddc74611f7529d550af34b0a1115866a5
Tomass Yoga <tomass.petersons96@gmail.com>
2022-05-23 17:04:40 +0300

    Add "palindromes.js"  TOP solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,14 +1,28 @@
 const palindromes = function (text) {
-  let reverseText = "";
-  text = text.toLowerCase();
-  text = text.replace(/[^\w\s\']|_/g, "");
-  text = text.replace(/\s/g, "");
-  for (i=text.length - 1; i >= 0; i--) {
-    reverseText += text[i];
-  }
-  const result = text === reverseText ? true : false;
-  return result;
+
+  // MY APPROACH
+
+  // let reverseText = "";
+  // text = text.toLowerCase();
+  // text = text.replace(/[^\w\s\']|_/g, "");
+  // text = text.replace(/\s/g, "");
+  // for (i=text.length - 1; i >= 0; i--) {
+  //   reverseText += text[i];
+  // }
+  // const result = text === reverseText ? true : false;
+  // return result;
+  
+  // SOLUTION APPROACH
+  processedString = text.toLowerCase().replace(/[^a-z]/g, "");
+    return (
+      processedString
+        .split("")
+        .reverse()
+        .join("") == processedString
+    );
 }
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
