2099e513d80f714f136dbdbfa8589c5cda632fc5
Tomass Yoga <tomass.petersons96@gmail.com>
2022-05-23 16:56:48 +0300

    Finish "palindromes.js" task

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
-
-};
+const palindromes = function (text) {
+  let reverseText = "";
+  text = text.toLowerCase();
+  text = text.replace(/[^\w\s\']|_/g, "");
+  text = text.replace(/\s/g, "");
+  for (i=text.length - 1; i >= 0; i--) {
+    reverseText += text[i];
+  }
+  const result = text === reverseText ? true : false;
+  return result;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
